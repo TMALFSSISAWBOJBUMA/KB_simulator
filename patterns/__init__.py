12630484d59025400f1d5c1f872a67995e5c5b9c
JakubPiech <piechjakub99@gmail.com>
2024-06-06 23:32:05 +0200

    vispy plotting updated

--- patterns/__init__.py
+++ patterns/__init__.py
@@ -7,12 +7,17 @@ import matplotlib
 from typing import TextIO, Optional
 import pathlib as pl
 
+try:
+    from vispy.plot import Fig
+    from vispy.plot import PlotWidget
+except ImportError:
+    print("Vispy not found, plotting disabled")
 
 # radiation patterns are stored as gain value (in dBi) in 2x360 matrices
 # row 0 stores gain parts for each azimuth, row 1 for each elevation angle from the main radiation direction
 # (pattern[0,i] + pattern[1,j]) represents gain for horizontal angle {i} and vertical angle {j}
-HALF_WAVE_DIPOLE = np.cos(np.radians(np.ogrid[:360])) ** 2
-HALF_WAVE_DIPOLE = 10 * np.log10(HALF_WAVE_DIPOLE) + 2.15  # dBi
+HALF_WAVE_DIPOLE = np.cos(np.radians(np.ogrid[:360]))
+HALF_WAVE_DIPOLE = 20 * np.log10(HALF_WAVE_DIPOLE) + 2.15  # dBi
 # hw_dipole_radiation = np.tile(hw_dipole_radiation, (360, 1))
 HALF_WAVE_DIPOLE = np.vstack([np.zeros(360), HALF_WAVE_DIPOLE])
 """Half-wave dipole radiation pattern"""
@@ -75,7 +80,7 @@ def visualize_pattern(
     plt_data: np.ndarray = pattern[0, angles // step] + pattern[
         1, angles // step
     ].reshape(-1, 1)
-    plt_data = plt_data.flatten()
+    plt_data = plt_data.flatten().astype(np.float32)
     plt_data = np.clip(plt_data, -80, 100)
     # clim = (plt_data.min(), plt_data.max())
     plt_data -= plt_data.min()
@@ -84,11 +89,45 @@ def visualize_pattern(
     vertices, faces = generate_mesh(angles)
     vertices *= plt_data.reshape(-1, 1)
 
+    ax.title.text = "3D radiation pattern"
     ax.mesh(vertices=vertices, faces=faces, vertex_colors=colour)
     # colorbar isn't visualized correctly, so skip for now
     # ax.colorbar(clim=clim, cmap=cmap, position='right',label='Gain [dBi]')
     return fig if plot is None else None
 
+def plot_flat_pattern(
+    pattern: np.ndarray, plot: Optional[PlotWidget] = None, cmap: Optional[str] = "jet"
+) -> None | Fig:
+    """Displays a 2d color plot of a radiation pattern
+
+    Args:
+        pattern (np.ndarray): array (2,N) of gain values
+        plot (PlotWidget, optional): parent plotwidget for the mesh.
+        If None a new `Fig` will be created.
+        cmap (str, optional): colormap name. Defaults to "jet".
+
+    Returns:
+        None | Fig: created figure if plot is None, else None
+    """
+    if plot is None:
+        fig = Fig(show=False)
+        ax: PlotWidget = fig[0, 0]
+    else:
+        ax = plot
+
+    step = 360 // pattern.shape[1]
+    angles = np.arange(-180, 181, step)
+    plt_data: np.ndarray = pattern[0, angles // step] + pattern[
+        1, angles // step
+    ].reshape(-1, 1)
+    plt_data = np.clip(plt_data.astype(np.float32), -80, 100)
+    clim = (plt_data.min(), plt_data.max())
+    ax.image(plt_data, cmap=cmap)
+    ax.colorbar(clim=[f"{x:.2f}" for x in clim], cmap=cmap, label='Gain [dBi]')
+    ax.title.text = "Flattened radiation pattern"
+    ax.xlabel.text = "Azimuth [deg]"
+    ax.ylabel.text = "Elevation [deg]"
+    return fig if plot is None else None
 
 def pattern_from_msi_file(fp: TextIO) -> np.ndarray:  # simple parser
     gain = 0.0
@@ -112,13 +151,15 @@ def pattern_from_msi_file(fp: TextIO) -> np.ndarray:  # simple parser
 
 
 def demo():
-    fig = Fig(show=False)
+    fig = Fig(show=False, title="Radiation patterns demo")
 
-    visualize_pattern(HALF_WAVE_DIPOLE[:, ::10], fig[0, 0])
+    visualize_pattern(HALF_WAVE_DIPOLE, fig[1, 0])
+    plot_flat_pattern(HALF_WAVE_DIPOLE, fig[1, 1])
 
     with open(pl.Path(__file__).parent.joinpath("-45 Port_898_T0.msi")) as f:
         pat = pattern_from_msi_file(f)
-    visualize_pattern(pat, fig[0, 1])
+    visualize_pattern(pat, fig[0, 0])
+    plot_flat_pattern(pat, fig[0, 1])
 
     fig.show(run=True)
 
